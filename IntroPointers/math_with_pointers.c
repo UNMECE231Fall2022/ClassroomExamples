3e4eed37c67f2e1c2fcff452e8ceafbd96d590b3
Francisco Viramontes <fviramontes8@unm.edu>
2022-09-06 12:07:09 -0600

    Add math_with_pointers
    Added an example of doing simple arithmetic with pointers

---
+++ IntroPointers/math_with_pointers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	int x = 5;
+	float y = 42.0f;
+	double z = 28794.1240;
+
+//	printf("x = %d, &x = %p\n", x, &x);
+//	printf("y = %f, &y = %p\n", y, &y);
+//	printf("z = %lf, &z = %p\n", z, &z);
+
+	int* a = &x;
+	float* b;
+	b = &y;
+	double *c;
+	c = &z;
+
+//	printf("*a = %d, a = %p, &a = %p\n", *a, a, &a);
+//	printf("*b = %f, b = %p, &b = %p\n", *b, b, &b);
+//	printf("*c = %lf, c = %p, &c = %p\n", *c, c, &c);
+
+	*a = 12;
+	printf("x = %d\n", x);
+
+	printf("x + y = %f\n", (float) (*a) + (*b));
+	printf("x * y = %f\n", (float) (*a) * (*b));
+
+	return 0;
+}
