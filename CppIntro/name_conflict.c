f95154a1dd1796c114e5199a593964f9bd60c5c5
Francisco Viramontes <fviramontes8@unm.edu>
2022-10-03 11:22:52 -0600

    Add name conflict example
    Added a C program that create two functions of the same name, in the C
    programming language this causes a name conflict, as the compiler cannot
    tell which function to use when the function is called.

---
+++ CppIntro/name_conflict.c
@@ -0,0 +1,17 @@
+void swap(int* x, int* y) {
+	int t;
+	t = *x;
+	*x = *y;
+	*y = t;
+}
+
+void swap(float* x, float* y) {
+	float t;
+	t = *x;
+	*x = *y;
+	*y = t;
+}
+
+int main() {
+	return 0;
+}
